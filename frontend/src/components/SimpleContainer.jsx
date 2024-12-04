8c4bbda6edd085285bce0e7371b5b7f424aa4ffb
Joao Mello <joaomgvitor@hotmail.com>
2024-12-03 21:26:36 -0300

    Recomeçando com novas ferramentas

--- frontend/src/components/SimpleContainer.jsx
+++
@@ -1,7 +0,0 @@
-export default function SimpleContainer({ children }) {
-    return (
-        <section className="mx-auto my-3 flex flex-col gap-2 w-[600px] max-w-[90vw] h-max">
-            {children}
-        </section>
-    );
-}
